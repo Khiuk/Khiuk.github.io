8697145728f36071145227581218b0019e316f91
Khiuk <42030173+Khiuk@users.noreply.github.com>
2020-03-24 17:10:06 +0700

    WebDev start

---
+++ WebDev/script.js
@@ -0,0 +1,16 @@
+'use strict'
+alert ('Это мой сайт, который я делал в течении учебы верстке веб-сайтов');
+alert ('Меня зовут Роман!');
+alert ('Рад познакомиться!');
+
+let admin, name;
+name = 'Джон';
+admin = name ;
+
+let planetName, currentUserName;
+planetName = 'Криминалиус';
+currentUserName = 'Anus';
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(birthday);
